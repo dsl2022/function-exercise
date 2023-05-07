59546c07445dcfe4a3b4455eb1c279809f9d2f93
LS <2015rpro@gmail.com>
2023-05-06 22:26:56 -0400

    Update medium.js

--- medium.js
+++ medium.js
@@ -2,7 +2,7 @@
 
 // Write a function that takes in an array of numbers and returns a new array with only the even numbers.
 function getEvenNumbers(numbers) {
-  // Your code here
+  return numbers.filter(number => number % 2 === 0);
 }
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -12,9 +12,14 @@ console.log(getEvenNumbers(numbers)); // Output: [2, 4, 6, 8, 10]
 
 // Write a function that takes in a string and returns a new string with the first letter of each word capitalized.
 function capitalizeFirstLetter(str) {
-  // Your code here
+  const words = str.split(' ');
+  const capitalizedWords = words.map(word => {
+    const firstLetter = word.charAt(0).toUpperCase();
+    const restOfWord = word.slice(1);
+    return firstLetter + restOfWord;
+  });
+  return capitalizedWords.join(' ');
 }
-
 const str = 'the quick brown fox jumps over the lazy dog';
 
 console.log(capitalizeFirstLetter(str)); // Output: 'The Quick Brown Fox Jumps Over The Lazy Dog'
@@ -22,8 +27,9 @@ console.log(capitalizeFirstLetter(str)); // Output: 'The Quick Brown Fox Jumps O
 
 // Write a function that takes in an array of objects representing people, and returns a new array with only the people who are over 30 years old.
 function getPeopleOver30(people) {
-  // Your code here
+  return people.filter(person => person.age > 30);
 }
+
 
 const people = [
     { name: 'John', age: 32 },
